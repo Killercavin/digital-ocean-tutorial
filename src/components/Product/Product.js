5e5d122326c07fbc142aeb2317caba229727d511
Cavin <cavinlarry001@gmail.com>
2024-07-29 13:47:01 +0300

    Update page by adding Product.js

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,88 +1,103 @@
-import React, {Component} from "react";
+import React, { useReducer } from "react";
 import './Product.css';
-// import product01 from './product-img-1.jpg';
+import product01 from './product-img-1.jpg';
+import product02 from './product-img-2.jpg';
+import product03 from './product-img-3.jpg';
+import product04 from './product-img-4.jpg';
+import product05 from './product-img-5.jpg';
+import product06 from './product-img-6.jpg';
+import product07 from './product-img-7.jpg';
+import product08 from './product-img-8.jpg';
+import product09 from './product-img-9.jpg';
+import product10 from './product-img-10.jpg';
+import product11 from './product-img-11.jpg';
+import product12 from './product-img-12.jpg';
 
-
-const products = [
-    {
-     emoji: '🍦',
-     name: 'ice cream',
-     price: 5
-    },
-    {
-     emoji: '🍩',
-     name: 'donuts',
-     price: 2.5,
-    },
-    {
-     emoji: '🍉',
-     name: 'watermelon',
-     price: 4
-    }
+const Product = () => {
+    const products = [
+        { name: 'Brujia Sev', productImg: product01, price: 125 },
+        { name: 'Nutri Choice', productImg: product02, price: 150 },
+        { name: 'Star Bites', productImg: product03, price: 175 },
+        { name: 'Lays', productImg: product04, price: 150 },
+        { name: 'Popcorns', productImg: product05, price: 100 },
+        { name: 'Epigamia', productImg: product06, price: 375 },
+        { name: 'Cheese Slices', productImg: product07, price: 375 },
+        { name: 'Corn Flakes', productImg: product08, price: 350 },
+        { name: 'Millet Pancake', productImg: product09, price: 245 },
+        { name: 'Amul Butter', productImg: product10, price: 425 },
+        { name: 'French Roast', productImg: product11, price: 280 },
+        { name: 'Cento', productImg: product12, price: 145 },
     ];
 
-class Product extends Component{
-
-    state = {
-        cart: [],
-        total: 0,
-    };
-
-    concurrencyOptions = {
+    const currencyOptions = {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
-    }
-    
-    getTotal = () =>{
-        return this.state.total.toLocaleString(undefined, this.concurrencyOptions);
+    };
+
+    const cartReducer = (state, action) => {
+        switch (action.type) {
+            case 'add':
+                return {
+                    cart: [...state.cart, action.product],
+                    total: state.total + action.product.price,
+                };
+
+            case 'remove':
+                const productIndex = state.cart.findIndex(item => item.name === action.product.name);
+                if (productIndex < 0) return state;
+                const updatedCart = [...state.cart];
+                updatedCart.splice(productIndex, 1);
+                return {
+                    cart: updatedCart,
+                    total: state.total - action.product.price,
+                };
+
+            default:
+                return state;
+        }
+    };
+
+    function getTotal(total) {
+        return total.toLocaleString(undefined, currencyOptions);
     }
 
-    add = (product) =>{
-        return (
-            this.setState(state => ({
-                cart: [...state.cart, product.name],
-                total: (state.total + product.price),
-            }))
-        );
+    const [state, dispatch] = useReducer(cartReducer, { cart: [], total: 0 });
+
+    function add(product) {
+        dispatch({ product, type: 'add' });
     }
 
-    remove = (product) => {
-        this.setState(state => {
-            const cart = [...state.cart];
-            cart.splice(cart.indexOf(product.name))
-            return ({
-                cart,
-                total: state.total - product.price
-            })
-        })
+    function remove(product) {
+        dispatch({ product, type: 'remove' });
     }
 
-    render(){
-        return (
-            <div className="product-wrapper">
-                <div>Shopping Cart: {this.state.cart.length} total items</div>
-                <div>Total: {this.getTotal()} items</div>
-                {/* <div className="product">
-                    <span role="img" aria-label="image">
-                    <img src={product01} alt="Product01" />
-                    </span>
-                    <button onClick={this.add}>Add</button>
-                    <button onClick={this.remove}>Remove</button>
-                </div> */}
-                <div>
-                    {products.map(product => (
-                        <div key={product.name}>
-                            <div className="product">
-                                <span role="img" aria-label="{product.name}">{product.emoji}</span>
-                                <button onClick={() => this.add(product)} className="product-btn">Add</button>
-                                <button onClick={() => this.remove(product)} className="product-btn">Remove</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+    return (
+        <div className="product-wrapper">
+            <div className="header clearfix">
+                <h1>Product Store</h1>
+                <div className="shop-cart">Total Products: {state.cart.length}</div>
+                <div className="shop-total">Total Price: ${getTotal(state.total)}</div>
             </div>
-        );
-    }
+            <div className="product-grid">
+                {products.map(product => (
+                    <div key={product.name} className="product-item">
+                        <div className="product-title">{product.name}</div>
+                        <div className="product">
+                            <div className="product-name"></div>
+                            <span role="img" aria-label={product.name}>
+                                <img className="product-img" src={product.productImg} alt={product.name} />
+                            </span>
+                            <div className="product-price">${product.price.toLocaleString(undefined, currencyOptions)}</div>
+                        </div>
+                        <div className="button-container">
+                            <button className="product-btn" onClick={() => add(product)}>Add</button>
+                            <button className="product-btn" onClick={() => remove(product)}>Remove</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Product;
